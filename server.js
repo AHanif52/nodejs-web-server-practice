74a14abfc103a1a747ac77198ec6107ba1a5596f
Mohammad Aufa Hanif <aufahanif85@gmail.com>
2024-07-19 16:51:24 +0700

    komen

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// console.log("Halo, kita akan membuat server");
 const http = require("http");
 
 // logika untuk menangani dan menaggapi request ditulisakan pada gfunsi ini
@@ -8,24 +7,45 @@ const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("content-Type", "text/html");
-  response.statusCode = 200;
 
   const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("<h1>Halo HTTP server!</h1>");
-  }
+  if (url === "/") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end("<h1>Ini adalah homepage</h1>");
+    } else {
+      response.statusCode = 400;
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end("<h1>Halaman About</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-  if (method === "POST") {
-    response.end("<h1>Hai! ini adalah method POST</h1>");
-  }
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
 
-  if (method === "PUT") {
-    response.end("<h1>Hai! ini adalah method PUT</h1>");
-  }
-
-  if (method === "DELETE") {
-    response.end("<h1>Hai! ini adalah method DELETE</h1>");
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.statusCode = 200;
+        response.end(`<h1>Hai, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      response.statusCode = 400;
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else {
+    response.statusCode = 404;
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
 
